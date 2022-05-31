c1293b9a874f50e4e03415ea9d7e6ad11a8ccd54
KADESH STEWART <kks9788@nyu.edu>
2022-05-31 04:51:53 -0400

    update website

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Something cool will happen here, soon.
         </p>
         <a
           className="App-link"
